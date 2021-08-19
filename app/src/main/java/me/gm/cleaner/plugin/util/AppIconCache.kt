4f05fad8cdbf542adb2ebc0fb16c281b17e7c159
GuhDoy <46047320+GuhDoy@users.noreply.github.com>
2021-08-19 14:50:58 +0800

    remove weird code from AppIconCache

--- app/src/main/java/me/gm/cleaner/plugin/util/AppIconCache.kt
+++ app/src/main/java/me/gm/cleaner/plugin/util/AppIconCache.kt
@@ -11,34 +11,20 @@ import kotlinx.coroutines.*
 import me.gm.cleaner.plugin.R
 import me.zhanghai.android.appiconloader.AppIconLoader
 import rikka.core.util.BuildUtils
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
 object AppIconCache : CoroutineScope {
 
-    private class AppIconLruCache constructor(maxSize: Int) :
-        LruCache<Triple<String, Int, Int>, Bitmap>(maxSize) {
-
-        override fun sizeOf(key: Triple<String, Int, Int>, bitmap: Bitmap): Int {
-            return bitmap.byteCount / 1024
-        }
-    }
-
     override val coroutineContext: CoroutineContext get() = Dispatchers.Main
-
-    private val lruCache: LruCache<Triple<String, Int, Int>, Bitmap>
 
     private val dispatcher: CoroutineDispatcher
 
-    private var appIconLoaders = mutableMapOf<Int, AppIconLoader>()
+    private var appIconLoaders = ConcurrentHashMap<Int, AppIconLoader>()
 
     init {
-        // Initialize app icon lru cache
-        val maxMemory = Runtime.getRuntime().maxMemory() / 1024
-        val availableCacheSize = (maxMemory / 4).toInt()
-        lruCache = AppIconLruCache(availableCacheSize)
-
         // Initialize load icon scheduler
         val availableProcessorsCount = try {
             Runtime.getRuntime().availableProcessors()
@@ -50,30 +36,8 @@ object AppIconCache : CoroutineScope {
         dispatcher = loadIconExecutor.asCoroutineDispatcher()
     }
 
-    fun dispatcher(): CoroutineDispatcher {
-        return dispatcher
-    }
-
-    private fun get(packageName: String, userId: Int, size: Int): Bitmap? {
-        return lruCache[Triple(packageName, userId, size)]
-    }
-
-    private fun put(packageName: String, userId: Int, size: Int, bitmap: Bitmap) {
-        if (get(packageName, userId, size) == null) {
-            lruCache.put(Triple(packageName, userId, size), bitmap)
-        }
-    }
-
-    private fun remove(packageName: String, userId: Int, size: Int) {
-        lruCache.remove(Triple(packageName, userId, size))
-    }
-
     @SuppressLint("NewApi")
     fun getOrLoadBitmap(context: Context, info: ApplicationInfo, userId: Int, size: Int): Bitmap {
-        val cachedBitmap = get(info.packageName, userId, size)
-        if (cachedBitmap != null) {
-            return cachedBitmap
-        }
         var loader = appIconLoaders[size]
         if (loader == null) {
             val shrinkNonAdaptiveIcons =
@@ -81,9 +45,7 @@ object AppIconCache : CoroutineScope {
             loader = AppIconLoader(size, shrinkNonAdaptiveIcons, context)
             appIconLoaders[size] = loader
         }
-        val bitmap = loader.loadIcon(info, false)
-        put(info.packageName, userId, size, bitmap)
-        return bitmap
+        return loader.loadIcon(info, false)
     }
 
     @JvmStatic
@@ -92,18 +54,6 @@ object AppIconCache : CoroutineScope {
     ): Job = launch {
         val size = view.measuredWidth.let {
             if (it > 0) it else context.resources.getDimensionPixelSize(R.dimen.large_icon_size)
-        }
-        val cachedBitmap = try {
-            withContext(dispatcher) {
-                get(info.packageName, userId, size)
-            }
-        } catch (e: CancellationException) {
-            // do nothing if canceled
-            return@launch
-        }
-        if (cachedBitmap != null) {
-            view.setImageBitmap(cachedBitmap)
-            return@launch
         }
 
         val bitmap = try {
