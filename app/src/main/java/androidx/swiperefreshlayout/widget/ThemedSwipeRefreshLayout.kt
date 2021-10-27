f7ebafd3ac377de4ab4f09d79965c03daf8c274d
GuhDoy <46047320+GuhDoy@users.noreply.github.com>
2021-10-27 17:09:05 +0800

    fix ProgressViewOffset

--- app/src/main/java/androidx/swiperefreshlayout/widget/ThemedSwipeRefreshLayout.kt
+++ app/src/main/java/androidx/swiperefreshlayout/widget/ThemedSwipeRefreshLayout.kt
@@ -2,39 +2,13 @@ package androidx.swiperefreshlayout.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import me.gm.cleaner.plugin.util.colorAccent
 
-open class ThemedSwipeRefreshLayout(context: Context, attrs: AttributeSet?) :
+class ThemedSwipeRefreshLayout(context: Context, attrs: AttributeSet?) :
     SwipeRefreshLayout(context, attrs) {
-    private fun init() {
-        setColorSchemeColors(context.colorAccent)
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val child = childView
-        if (child != null) {
-            measureChild(child, widthMeasureSpec, heightMeasureSpec)
-            setMeasuredDimension(
-                child.measuredWidth + paddingLeft + paddingRight,
-                child.measuredHeight + paddingTop + paddingBottom
-            )
-        }
-    }
-
-    private val childView: View?
-        get() {
-            for (i in 0 until childCount) {
-                val child = getChildAt(i)
-                if (child != mCircleView) {
-                    return child
-                }
-            }
-            return null
-        }
 
     init {
-        init()
+        setColorSchemeColors(context.colorAccent)
+        setProgressViewOffset(false, 0, progressViewEndOffset)
     }
 }
