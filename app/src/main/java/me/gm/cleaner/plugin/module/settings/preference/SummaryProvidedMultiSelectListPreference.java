59f5dbdc73687ec411be6f643e43368a1454fbd1
GuhDoy <46047320+GuhDoy@users.noreply.github.com>
2021-12-03 10:26:18 +0800

    better sequence

--- app/src/main/java/me/gm/cleaner/plugin/module/settings/preference/SummaryProvidedMultiSelectListPreference.java
+++ app/src/main/java/me/gm/cleaner/plugin/module/settings/preference/SummaryProvidedMultiSelectListPreference.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Green Mushroom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.gm.cleaner.plugin.module.settings.preference;
 
 import android.annotation.SuppressLint;
@@ -9,7 +25,6 @@ import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.MultiSelectListPreference;
 import androidx.preference.R;
 
-import java.text.Collator;
 import java.util.Arrays;
 
 @SuppressLint({"RestrictedApi", "PrivateResource"})
@@ -65,10 +80,9 @@ public class SummaryProvidedMultiSelectListPreference extends MultiSelectListPre
                 return (preference.getContext().getString(R.string.not_set));
             } else {
                 var entryValues = Arrays.asList(preference.getEntryValues());
-                var collator = Collator.getInstance();
-                var values = preference.getValues().stream()
+                var values = Arrays.stream(preference.getEntryValues())
+                        .filter(it -> preference.getValues().contains(it))
                         .map(it -> preference.getEntries()[entryValues.indexOf(it)])
-                        .sorted(collator)
                         .toArray();
                 return ListFormatter.getInstance().format(values);
             }
