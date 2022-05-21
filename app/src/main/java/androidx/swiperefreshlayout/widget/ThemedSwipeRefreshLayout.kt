e605d076a3f207bcb1638ae586c3dba8bdc83ac6
GM <897662476@qq.com>
2022-05-21 17:36:16 +0800

    material3 swipeRefreshLayoutProgressSpinnerBackgroundColor & FastScrollerThumbColor

--- app/src/main/java/androidx/swiperefreshlayout/widget/ThemedSwipeRefreshLayout.kt
+++ app/src/main/java/androidx/swiperefreshlayout/widget/ThemedSwipeRefreshLayout.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Green Mushroom
+ * Copyright 2022 Green Mushroom
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +14,30 @@
  * limitations under the License.
  */
 
-package me.gm.cleaner.plugin.widget
+package androidx.swiperefreshlayout.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.ShapeDrawable
 import android.util.AttributeSet
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
+import com.google.android.material.R
 import me.gm.cleaner.plugin.ktx.colorPrimary
+import me.gm.cleaner.plugin.ktx.colorSurface
 
+@SuppressLint("PrivateResource")
 class ThemedSwipeRefreshLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : SwipeRefreshLayout(context, attrs) {
 
     init {
+        val overlayColor = ContextCompat
+            .getColorStateList(context, R.color.m3_popupmenu_overlay_color)!!
+            .defaultColor
+        val backgroundColor = ColorUtils.compositeColors(overlayColor, context.colorSurface)
+        (mCircleView.background as ShapeDrawable).paint.color = backgroundColor
+
         setColorSchemeColors(context.colorPrimary)
     }
 }
