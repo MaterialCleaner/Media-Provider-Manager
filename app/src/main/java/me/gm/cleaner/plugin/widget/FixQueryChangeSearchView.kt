28473b53305ed069f768fd1fa1132d63f032ebd4
GM <897662476@qq.com>
2022-09-21 22:16:15 +0800

    FixQueryChangeSearchView

---
+++ app/src/main/java/me/gm/cleaner/plugin/widget/FixQueryChangeSearchView.kt
@@ -0,0 +1,36 @@
+package me.gm.cleaner.plugin.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.AttrRes
+
+class FixQueryChangeSearchView : FixLayoutSearchView {
+    var shouldIgnoreQueryChange = false
+        private set
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, @AttrRes defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    )
+
+    override fun setIconified(iconify: Boolean) {
+        shouldIgnoreQueryChange = true
+        super.setIconified(iconify)
+        shouldIgnoreQueryChange = false
+    }
+
+    override fun onActionViewCollapsed() {
+        shouldIgnoreQueryChange = true
+        super.onActionViewCollapsed()
+        shouldIgnoreQueryChange = false
+    }
+
+    override fun onActionViewExpanded() {
+        shouldIgnoreQueryChange = true
+        super.onActionViewExpanded()
+        shouldIgnoreQueryChange = false
+    }
+}
