ebdec8fe1e18b9b136051899fe2603a623dcc1e3
GuhDoy <46047320+GuhDoy@users.noreply.github.com>
2023-03-14 00:57:38 +0800

    protect mount point

--- app/src/main/cpp/bpf_hook.cpp
+++ app/src/main/cpp/bpf_hook.cpp
@@ -30,17 +30,38 @@ const std::regex PATTERN_OWNED_PATH(
         std::regex_constants::icase);
 static constexpr char PRIMARY_VOLUME_PREFIX[] = "/storage/emulated";
 
+bool isPackageOwnedPath(const std::string &path) {
+    return path.length() >= 33 && std::regex_match(path, PATTERN_OWNED_PATH);
+}
+
 bool (*old_StartsWith)(std::string_view s, std::string_view prefix);
 
 bool new_StartsWith(std::string_view s, std::string_view prefix) {
     if (prefix == PRIMARY_VOLUME_PREFIX) {
         auto path = std::string(s);
-        std::smatch match;
-        if (std::regex_match(path, match, PATTERN_OWNED_PATH)) {
+        if (isPackageOwnedPath(path)) {
             return false;
         }
     }
     return old_StartsWith(s, prefix);
+}
+
+bool (*old_containsMount)(const std::string &path);
+
+bool new_containsMount(const std::string &path) {
+    if (isPackageOwnedPath(path)) {
+        return true;
+    }
+    return old_containsMount(path);
+}
+
+bool (*old_containsMount_30)(const std::string &path, const std::string &userid);
+
+bool new_containsMount_30(const std::string &path, const std::string &userid) {
+    if (isPackageOwnedPath(path)) {
+        return true;
+    }
+    return old_containsMount_30(path, userid);
 }
 
 namespace bpf_hook {
@@ -58,16 +79,34 @@ namespace bpf_hook {
     }
 
     void Hook(void *handle, HookFunType hook_func) {
-        if (GetApiLevel() < 31) {
-            return;
-        }
         if (!IsFuse()) {
             return;
         }
-        auto startsWith = dlsym(handle,
-                                "_ZN7android4base10StartsWithENSt6__ndk117basic_string_viewIcNS1_11char_traitsIcEEEES5_");
-        if (startsWith != nullptr) {
-            hook_func((void *) startsWith, (void *) new_StartsWith, (void **) &old_StartsWith);
+        // bypass restriction
+        if (GetApiLevel() >= 31) {
+            auto startsWith = dlsym(handle,
+                                    "_ZN7android4base10StartsWithENSt6__ndk117basic_string_viewIcNS1_11char_traitsIcEEEES5_");
+            if (startsWith != nullptr) {
+                hook_func((void *) startsWith, (void *) new_StartsWith, (void **) &old_StartsWith);
+            } else {
+                LOGE("failed to find StartsWith");
+            }
+        }
+        // protect mount point
+        auto containsMount = dlsym(handle,
+                                   "_ZN13mediaprovider4fuse13containsMountERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE");
+        if (containsMount != nullptr) {
+            hook_func((void *) containsMount, (void *) new_containsMount,
+                      (void **) &old_containsMount);
+        } else {
+            auto containsMount_30 = dlsym(handle,
+                                          "_ZN13mediaprovider4fuse13containsMountERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEES9_");
+            if (containsMount_30 != nullptr) {
+                hook_func((void *) containsMount_30, (void *) new_containsMount_30,
+                          (void **) &old_containsMount_30);
+            } else {
+                LOGE("failed to find containsMount");
+            }
         }
     }
 }  // namespace bpf_hook
