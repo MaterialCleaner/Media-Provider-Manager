d3524a48bda2662f76d8c25ffafc90dadb050893
GuhDoy <greenmushroomcn@gmail.com>
2024-04-05 09:04:54 +0800

    move reduceScaledMinimumScalingSpan to App

--- app/src/main/java/me/gm/cleaner/plugin/app/App.kt
+++ app/src/main/java/me/gm/cleaner/plugin/app/App.kt
@@ -16,9 +16,11 @@
 
 package me.gm.cleaner.plugin.app
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.os.Build
+import android.view.ViewConfiguration
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
@@ -39,5 +41,18 @@ class App : Application() {
         RootPreferences.init(createDeviceProtectedStorageContext())
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         DynamicColors.applyToActivitiesIfAvailable(this)
+        reduceScaledMinimumScalingSpan()
+    }
+
+    @SuppressLint("BlockedPrivateApi")
+    private fun reduceScaledMinimumScalingSpan() {
+        // Reduce ScaledMinimumScalingSpan due to the following reasons:
+        // 1) I think it significantly reduces the user experience.
+        // 2) Many Google apps do not respect this value.
+        // 3) The creator of pinch-to-zoom did not specify this value.
+        val field = ViewConfiguration::class.java.getDeclaredField("mMinScalingSpan")
+        field.isAccessible = true
+        val viewConfiguration = ViewConfiguration.get(this)
+        field.set(viewConfiguration, viewConfiguration.scaledTouchSlop * 2)
     }
 }
